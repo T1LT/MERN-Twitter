147cc0abfab6e9dd745ab3aae576bd80b9e70b0c
Nishant Racherla <nishant.racherla@gmail.com>
2023-01-14 23:14:44 -0800

    added validations

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -6,6 +6,8 @@ const User = mongoose.model("User");
 const passport = require("passport");
 const { loginUser, restoreUser } = require("../../config/passport");
 const { isProduction } = require("../../config/keys");
+const validateRegisterInput = require("../../validations/register");
+const validateLoginInput = require("../../validations/login");
 
 // TEST ROUTE
 router.get("/", (req, res, next) => {
@@ -15,7 +17,7 @@ router.get("/", (req, res, next) => {
 });
 
 // POST /api/users/register
-router.post("/register", async (req, res, next) => {
+router.post("/register", validateRegisterInput, async (req, res, next) => {
   const user = await User.findOne({
     $or: [{ email: req.body.email }, { username: req.body.username }],
   });
@@ -55,7 +57,7 @@ router.post("/register", async (req, res, next) => {
 });
 
 // POST /api/users/login
-router.post("/login", async (req, res, next) => {
+router.post("/login", validateLoginInput, async (req, res, next) => {
   passport.authenticate("local", async function (err, user) {
     if (err) return next(err);
     if (!user) {
