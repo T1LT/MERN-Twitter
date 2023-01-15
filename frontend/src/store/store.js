d8365092ee79ba5d0bbd9713153d74d610c1c90a
Nishant Racherla <nishant.racherla@gmail.com>
2023-01-15 00:27:33 -0800

    added most of the code for frontend

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,7 +1,12 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import sessionReducer from "./session";
+import errorsReducer from "./errors"
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  session: sessionReducer,
+  errors: errorsReducer
+});
 
 let enhancer;
 
