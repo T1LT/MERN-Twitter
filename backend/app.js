d5805265cdda6d89ee882a9851213cabf7936079
Nishant Racherla <nishant.racherla@gmail.com>
2023-01-14 22:40:18 -0800

    added user registration

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,8 @@ const cors = require("cors");
 const csurf = require("csurf");
 const { isProduction } = require("./config/keys");
 
+require("./models/User");
+
 const usersRouter = require("./routes/api/users");
 const tweetsRouter = require("./routes/api/tweets");
 const csrfRouter = require("./routes/api/csrf");
